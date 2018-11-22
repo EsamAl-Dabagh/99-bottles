31791332dc5705f954e3b37d8a8565507f717213
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-11-22 10:33:15 +0000

    feat: create Bottles class, create verse method

---
+++ test/bottles_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require_relative "../lib/bottles"
+
+class BottlesTest < Minitest::Test
+  def test_the_first_verse
+    expected = <<-VERSE 
+99 bottles of beer on the wall, 99 bottles of beer.
+Take one down and pass it around, 98 bottles of beer on the wall.
+VERSE
+    
+    assert_equal expected, ::Bottles.new.verse(99)
+  end
+end
