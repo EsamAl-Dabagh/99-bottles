b4ad57dd3cd3335f9b634c2cd7b46f29a55cde67
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-11-22 10:42:22 +0000

    feat: #verse handles penultimate verse

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -18,4 +18,12 @@ VERSE
     assert_equal expected, ::Bottles.new.verse(89)
   end
   
+  def test_verse_2
+    expected = <<-VERSE
+2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.
+    VERSE
+    assert_equal expected, ::Bottles.new.verse(2)
+  end
+
 end
