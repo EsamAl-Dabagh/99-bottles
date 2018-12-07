33e992fff116d8ec791a3a84e4534ddb8712ece5
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-12-07 08:04:02 +0000

    refactor: remove argument from BottleNumber#action

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -28,7 +28,7 @@ class Bottles
   end
 
   def action(number)
-    BottleNumber.new(number).action(number)
+    BottleNumber.new(number).action
   end
   
   def successor(number)
@@ -69,7 +69,7 @@ class BottleNumber
     end
   end
 
-  def action(number)
+  def action
     if number == 0
       "Go to the store and buy some more"
     else
